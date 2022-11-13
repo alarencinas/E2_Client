39e9c63094889def6d6280019d382c4a1132b073
ibarra18 <javier.ibarra@opendeusto.es>
2022-11-13 17:22:38 +0100

    main

--- src/client/MainProgram.java
+++ src/client/MainProgram.java
@@ -1,6 +1,17 @@
 package client;
 
-
+import client.controller.CrController;
+import client.controller.LoginController;
+import client.gui.*;
+import client.remote.*;
 public class MainProgram {
-	
+	public static void main(String[] args) {	 
+		ServiceLocator serviceLocator = new ServiceLocator();
+		serviceLocator.setService(args[0], args[1], args[2]);
+		LoginController loginController = new LoginController(serviceLocator);
+		LoginDialog loginDialog = new LoginDialog(loginController);			
+		CrController crController = new CrController(serviceLocator);
+		//Login
+		loginDialog.login();
+	}
 }
