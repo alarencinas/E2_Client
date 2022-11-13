1bebc7a9ca10d6d43e3a4f1d34e1520a54e122dc
alarencinas <alarencinas@mbp-de-alar.home>
2022-11-13 19:19:37 +0100

    Main window

---
+++ src/client/gui/ClientMainWindow.java
@@ -0,0 +1,84 @@
+package client.gui;
+
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class ClientMainWindow extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClientMainWindow frame = new ClientMainWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ClientMainWindow() {
+		setTitle("STRAVA MENU");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JButton btnChallenge = new JButton("CREATE CHALLENGE");
+		panel.add(btnChallenge);
+		
+		JButton btnSession = new JButton("CREATE SESSION");
+		panel.add(btnSession);
+		
+		JPanel panelSouth = new JPanel();
+		contentPane.add(panelSouth, BorderLayout.SOUTH);
+		
+		JButton btnExit = new JButton("Exit");
+		panelSouth.add(btnExit);
+		
+		btnChallenge.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO 
+				
+			}
+		});
+		btnSession.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO 
+				
+			}
+		});
+	}
+
+}
