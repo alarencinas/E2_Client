15621e6073cc86bc77cf5625d60c982de9c66633
alarencinas <alarencinas@mbp-de-alar.home>
2022-11-13 19:20:10 +0100

    mianWindow

--- src/client/gui/ClientMainWindow.java
+++ src/client/gui/ClientMainWindow.java
@@ -12,7 +12,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JLabel;
 
 public class ClientMainWindow extends JFrame {
 
